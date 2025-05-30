2ba030e293515a9109638b7d5e44ad3b6ddbd8e4
denwaya34 <denwaya34@gmail.com>
2025-05-30 23:45:50 +0900

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import App from './App.tsx';
 
 createRoot(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  document.getElementById('root')!
+  document.getElementById('root')!,
 ).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
