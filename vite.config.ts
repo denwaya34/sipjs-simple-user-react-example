dc1c6ca980534e2cb4fac06fc7f4743e9bbed62b
denwaya34 <denwaya34@gmail.com>
2025-05-30 23:20:40 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import checker from "vite-plugin-checker";
+import {resolve} from "node:path";
+
+const __dirname = import.meta.dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+      },
+    },
+  },
+  server: {
+    open: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
