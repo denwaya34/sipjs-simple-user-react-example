eb019ae2ae829ec4b564470d889af54b603b4f0f
denwaya34 <denwaya34@gmail.com>
2025-05-31 00:06:52 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-named-as-default
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
