20d8865e006c4d798d581d332eb3ddf2b5f05f54
denwaya34 <denwaya34@gmail.com>
2025-05-31 05:56:34 +0900

    init

--- src/env/client.ts
+++ src/env/client.ts
@@ -7,6 +7,7 @@ export const env = createEnv({
     VITE_SIP_URL: z.string().optional(),
     VITE_SIP_USER_NAME: z.string().optional(),
     VITE_SIP_USER_PASSWORD: z.string().optional(),
+    VITE_CALL_TO_URL: z.string().optional(),
   },
   runtimeEnv: import.meta.env,
   emptyStringAsUndefined: true,
